45ccc02a2e55fd3a38d4df131a446da2f466f130
Aya1712 <anbaoui.aya@gmail.com>
2023-10-30 16:58:24 +0100

    esercizio s2/g1(m2)

---
+++ m2/s1/progetto/D4.js
@@ -0,0 +1,6 @@
+let yellow = document.getElementById("yellow");
+let showButton = document.getElementById("showButton"); // Assumi che ci sia un pulsante con id "showButton" nel tuo HTML
+
+showButton.addEventListener("click", function() {
+    myIframe.style.display = "block"; // Rendi visibile il tuo iframe
+});
