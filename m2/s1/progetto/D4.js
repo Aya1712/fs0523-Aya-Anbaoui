fb62de50e981a3aeea7bf338f9a8bd583e6c3752
Aya1712 <anbaoui.aya@gmail.com>
2023-10-30 17:35:10 +0100

    push.css

--- m2/s1/progetto/D4.js
+++
@@ -1,6 +0,0 @@
-let yellow = document.getElementById("yellow");
-let showButton = document.getElementById("showButton"); // Assumi che ci sia un pulsante con id "showButton" nel tuo HTML
-
-showButton.addEventListener("click", function() {
-    myIframe.style.display = "block"; // Rendi visibile il tuo iframe
-});
